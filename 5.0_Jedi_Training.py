6a41c6db7c9e2a180e3f72d76491de893a37cd05
thawngbaw <thawngbaw@urbandaleschools.com>
2020-10-21 09:48:24 -0500

    jedi training, chapter 5

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -3,13 +3,13 @@
 '''
  1. Make the following program work.
    '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
+print("This program takes three numbers and returns the sum.")
+total = 0
 
-     for i in range(3):
-         x = int(input("Enter a number: "))
-         total += x
-     print("The total is:", total)
+for i in range(3):
+    x = int(input("Enter a number: "))
+    total += x
+print("The total is:", total)
   
 
 
